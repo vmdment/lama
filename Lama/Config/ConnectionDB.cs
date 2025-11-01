4db2908ea7debad7f6d53f0941974a9ac7c89749
vmdment <vimaqeiz@gmail.com>
2025-10-31 21:31:57 -0500

    create first steps project lama

---
+++ Lama/Config/ConnectionDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace Lama.Config
+{
+    public class ConnectionDB
+    {
+        public string ConnectionString { get; set; }
+        public ConnectionDB(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public SqlDataReader Find(string query) {
+            
+            using(SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                return command.ExecuteReader();
+            }            
+        }
+        public int Execute(string query) {
+            using(SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected;
+            }
+        }
+    }
+}
---
+++ Lama/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lama.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Members")]
+        public LinkedList<Object> GetMembers()
+        {
+            LinkedList<Object> members = new LinkedList<Object>();
+            members.Append(new { nombre = "Luis", edad = 24});
+            members.Append(new { nombre = "Luis", edad = 25 });
+            return members;
+        }
+    }
+}
