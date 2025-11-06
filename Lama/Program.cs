fb44f648dd2d2ca2b0067ce531b67960a4daf7c0
vmdment <vimaqeiz@gmail.com>
2025-11-06 00:59:22 -0500

    chore: Change cors backend

--- Lama/Program.cs
+++ Lama/Program.cs
@@ -13,7 +13,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
